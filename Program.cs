a76764429e222c2a2a5e8a5583592d75b7e3c016
Vibin Varghese <91533254+WIT-Vibin-2021@users.noreply.github.com>
2022-11-22 12:01:55 +0000

    Robot Moving - Basic Function

--- Program.cs
+++ Program.cs
@@ -2,14 +2,54 @@
 
 namespace MarsRobot
 {
-    internal class Program
+    class Robot
     {
-        public int x { get; set; } // Robot x-axis
+        public int x { get; set; } // Robot x-axis 
         public int y { get; set; } // Robot y-axis
-                
 
         public string dir; // Moving direction
         string[] str_dir = { "L", "R", "F" };
+        private int row_max;
+        private int col_max;
+
+        public Robot(int row, int col)
+        {            
+            row_max = row;
+            col_max = col;
+
+            x = 1; // Initial Position
+            y = 1; // Initial Position
+        }
+
+        public void robot_moving(string command)
+        {
+            char[] moveCommand = command.ToCharArray();
+            for(int i=0; i<= moveCommand.Length;i++)
+            {
+                if (moveCommand[i]=='L')
+                {
+                    //Move Left
+                }
+                else if (moveCommand[i] == 'R')
+                {
+                    //Move Right
+                }
+                else if (moveCommand[i] == 'F')
+                {
+                    //Move forward
+                }
+                else
+                {
+                    Console.WriteLine("Invalid Command Value !!");
+                }
+
+            }
+        }
+
+    }
+    internal class Program
+    {
+        
 
         static void Main(string[] args)
         {
@@ -22,6 +62,9 @@ namespace MarsRobot
             int col = Int32.Parse(str[1]);
             Console.WriteLine(row);
             Console.WriteLine(col);
+            Robot robotObj = new  Robot( row, col);
         }
+
+       
     }
 }
