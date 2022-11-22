2184b8d4dda8c1e6dd23160021b81080253884a2
Vibin Varghese <91533254+WIT-Vibin-2021@users.noreply.github.com>
2022-11-22 12:57:02 +0000

    Complete

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace MarsRobot
         public int y { get; set; } // Robot y-axis
 
         public string dir; // Moving direction
-        string[] str_dir = { "NORTH", "EAST", "SOUTH","WEST" };
+        //string[] str_dir = { "NORTH", "EAST", "SOUTH","WEST" };
         private int row_max;
         private int col_max;
 
@@ -24,21 +24,23 @@ namespace MarsRobot
 
         public void robot_moving_forward()
         {
-            switch(dir)
+            // Considering Y - Axis for North-South
+            // Considering x - Axis for East-West
+            switch (dir)
             {
-                case "NORTH":
+                case "NORTH":  // Add Y Axis to move North
                     if (y < row_max)
                         y++;
                     break;
-                case "SOUTH":
-                    if(y>0)
+                case "SOUTH":  // Reduce Y Axis to move South (opposit of North)
+                    if (y>0)
                         y--;
                     break;
-                case "EAST":
+                case "EAST": // Add X Axis to move East
                     if (x < col_max)
                         x++;
                     break;
-                case "WEST":
+                case "WEST": // Reduce X Axis to move West (opposit of East)
                     if (x > 0)
                         x--;
                     break;
@@ -47,18 +49,54 @@ namespace MarsRobot
             }
 
         }
+        /* 
+         North + Left = West
+         North + Right = East
+
+         South + Left = East
+         South + Right = West
+
+         East + Left = North
+         East + Right =South
+
+         West + Left = South
+         West + Right = North
+        */
+
+        public void robot_moving_left()
+        {
+            if (dir == "NORTH")
+                dir = "WEST";
+            else if (dir == "SOUTH")
+                dir = "EAST";
+            else if (dir == "EAST")
+                dir = "NORTH";
+            else if (dir == "WEST")
+                dir = "SOUTH";
+        }
+        public void robot_moving_right()
+        {
+            if (dir == "NORTH")
+                dir = "EAST";
+            else if (dir == "SOUTH")
+                dir = "WEST";
+            else if (dir == "EAST")
+                dir = "SOUTH";
+            else if (dir == "WEST")
+                dir = "NORTH";
+        }
         public void robot_moving(string command)
         {
             char[] moveCommand = command.ToCharArray();
-            for(int i=0; i<= moveCommand.Length;i++)
+            for(int i=0; i< moveCommand.Length;i++)
             {
                 if (moveCommand[i]=='L')
                 {
-                    //Move Left
+                    robot_moving_left();
                 }
                 else if (moveCommand[i] == 'R')
                 {
-                    //Move Right
+                    robot_moving_right();
                 }
                 else if (moveCommand[i] == 'F')
                 {
@@ -89,6 +127,8 @@ namespace MarsRobot
             Console.WriteLine(row);
             Console.WriteLine(col);
             Robot robotObj = new  Robot( row, col);
+            robotObj.robot_moving(s2);
+            Console.WriteLine(robotObj.x + " " + robotObj.y + " " + robotObj.dir);
         }
 
        
