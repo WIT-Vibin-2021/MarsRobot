931492a9d59c8cbb390fd6e127d4d80388b89a04
Vibin Varghese <91533254+WIT-Vibin-2021@users.noreply.github.com>
2022-11-22 11:41:49 +0000

    BasicStructure
    BasicStructure

--- Program.cs
+++ Program.cs
@@ -4,9 +4,24 @@ namespace MarsRobot
 {
     internal class Program
     {
+        public int x { get; set; } // Robot x-axis
+        public int y { get; set; } // Robot y-axis
+                
+
+        public string dir; // Moving direction
+        string[] str_dir = { "L", "R", "F" };
+
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+            string s1 = Console.ReadLine(); // Grid Input
+            string s2 = Console.ReadLine(); // Command Input
+
+            string[] str = s1.Split("x");
+            int row = Int32.Parse(str[0]);
+            int col = Int32.Parse(str[1]);
+            Console.WriteLine(row);
+            Console.WriteLine(col);
         }
     }
 }
